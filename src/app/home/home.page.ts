e1f1eaf22d3fa457130e6c5dcf16a2f2005bf625
Simply Higgz <bryanhiggz1@gmail.com>
2021-09-17 01:29:35 +0300

    working code to get offerings

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Purchases } from '@ionic-native/purchases/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  offerings: any;
+  show: any;
 
-  constructor() {}
+  constructor(private purchases: Purchases) {
+    this.getOfferings();
+  }
+
+  getOfferings() {
+    this.offerings = this.purchases.getOfferings();
+    if (this.offerings.current != null) {
+      this.show = this.offerings.current;
+      console.log('yes!', this.show);
+    } else {
+      console.log('somethinsg missing');
+    }
+
+
+  }
 
 }
