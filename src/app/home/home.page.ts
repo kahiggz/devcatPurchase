7439dc7b5acb8c2654b5c75b5b7ee779683dfb2c
Simply Higgz <bryanhiggz1@gmail.com>
2021-09-17 11:06:38 +0300

    loading indicator and UI change

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Purchases } from '@ionic-native/purchases/ngx';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,17 +11,32 @@ export class HomePage {
 
   Offerings: any;
 
-  constructor(private purchases: Purchases) {
+  constructor(
+    private purchases: Purchases,
+    public loadingController: LoadingController) {
+  }
+
+  ionViewDidEnter(){
     this.getOfferings();
   }
 
   async getOfferings() {
+
+    const loading = await this.loadingController.create({
+      message: 'Please wait...',
+    });
+    loading.present();
+
     const offerings = await this.purchases.getOfferings();
     if (offerings.current !== null) {
       this.Offerings = offerings.current.availablePackages;
       console.log('yes!', offerings.current.availablePackages);
+      loading.dismiss();
+
     } else {
       console.log('somethinsg missing');
+      loading.dismiss();
+
     }
 
 
